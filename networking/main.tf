e4259ccdfe540f7770fa8be4b3ee715344bbf392
EC2 Default User <ec2-user@ip-172-31-19-199.ap-southeast-2.compute.internal>
2022-02-10 10:50:21 +0000

    75 VPC Subnets Terraform Apply Yourself

--- networking/main.tf
+++ networking/main.tf
@@ -26,3 +26,15 @@ resource "aws_subnet" "mtc_public_subnet" {
         Name = "mtc_public_${count.index + 1}"
     }
 }
+
+resource "aws_subnet" "mtc_private_subnet" {
+    count = length(var.private_cidrs)
+    vpc_id = aws_vpc.mtc_vpc.id
+    cidr_block = var.private_cidrs[count.index]
+    map_public_ip_on_launch = false
+    availability_zone = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"][count.index]
+    
+    tags = {
+        Name = "mtc_private_${count.index + 1}"
+    }
+}
--- networking/variables.tf
+++ networking/variables.tf
@@ -6,5 +6,8 @@ variable "vpc_cidr" {
 
 variable "public_cidrs" {
     type = list
-    
+}
+
+variable "private_cidrs" {
+    type = list
 }
