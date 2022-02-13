3654c7363bee82408d36bf6c490fe88f91137b8b
EC2 Default User <ec2-user@ip-172-31-19-199.ap-southeast-2.compute.internal>
2022-02-13 06:00:30 +0000

    90 ALB Initial Setup

--- main.tf
+++ main.tf
@@ -14,16 +14,16 @@ module "networking" {
 }
 
 
-module "database" {
-  source                 = "./database"
-  db_storage             = 10
-  db_engine_version      = "5.7.22"
-  db_instance_class      = "db.t2.micro"
-  dbname                 = var.dbname
-  dbuser                 = var.dbuser
-  dbpassword             = var.dbpassword
-  db_identifier          = "mtc-db"
-  skip_db_snapshot       = true
-  db_subnet_group_name   = module.networking.db_subnet_group_name[0]
-  vpc_security_group_ids = module.networking.db_security_group
-}
+# module "database" {
+#   source                 = "./database"
+#   db_storage             = 10
+#   db_engine_version      = "5.7.22"
+#   db_instance_class      = "db.t2.micro"
+#   dbname                 = var.dbname
+#   dbuser                 = var.dbuser
+#   dbpassword             = var.dbpassword
+#   db_identifier          = "mtc-db"
+#   skip_db_snapshot       = true
+#   db_subnet_group_name   = module.networking.db_subnet_group_name[0]
+#   vpc_security_group_ids = module.networking.db_security_group
+# }
